35cefce90f96f441dfdd50ce4bb3cee772a257ce
Manohar <149131741+Nani040905@users.noreply.github.com>
2026-06-19 19:01:19 +0530

    Fix module resolution for Zustand by adding moduleDirectories to Jest configuration

--- testing/js/jest.config.js
+++ testing/js/jest.config.js
@@ -16,6 +16,7 @@ module.exports = {
     '^llama.rn$': '<rootDir>/__mocks__/llama.rn.js',
     '^@react-native-async-storage/async-storage$': '<rootDir>/__mocks__/@react-native-async-storage/async-storage.js'
   },
+  moduleDirectories: ['node_modules', '<rootDir>/node_modules'],
   transform: {
     '^.+\\.jsx?$': 'babel-jest'
   }
